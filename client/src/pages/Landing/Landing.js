c8c1f9abf38700bc769f351dc0fac614bf43834a
Kate <kate.stark13@sbcglobal.net>
2018-04-21 11:42:57 -0500

    added age verification logic

--- client/src/pages/Landing/Landing.js
+++ client/src/pages/Landing/Landing.js
@@ -4,28 +4,32 @@ import AgeGate from 'agegate';
 class Landing extends React.Component {
 	/*setting initial values of this.state.year*/
 	state = {
-		year: " "
+		year: ""
 	};
 	/*handle changes to the input fields*/
 	handleInputChange = event => {
 		//pull the year value property off of the event.target (the element which triggered the event )
-		const {year, value} = event.target
+		const value = event.target.value;
+		console.log(value)
 		//set the state for the appropriate input field
 		this.setState({
-			[year]: value
+			year: value
 		});
 	};
 	//when the form is submitted, prevent the defualt even and alert the year
 	handleFormSubmit = event => {
 		event.preventDefault();
-
+		console.log('submit: ' + this.state.year)
+		if(this.state.year > 1997) {
+			alert ('Age check failed!')
+		} else {
+			window.location= "http://github.com/kstark13"
+		}
 	}
 
 
 	render() {
 		return(
-	
-
 			<form>
 				<h1>What year were you born? </h1>
 				<input 
